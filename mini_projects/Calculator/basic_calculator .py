244cb575bdededf307a933e9a3988c6f5e3ebc78
Somya Kothari <somyakothari454@gmail.com>
2025-08-26 05:06:49 -0700

    Create basic_calculator .py

---
+++ mini_projects/Calculator/basic_calculator .py
@@ -0,0 +1,24 @@
+# ******************Calculator***************************#
+a=int(input())
+b=int(input())
+add=a+b
+sub=a-b
+product=a*b
+division=a/b
+modulus=a%b
+exponential=a**b
+floor_division=a//b
+print("The \"sum\" of and b is:")
+print(add)
+print("The \"subtraction\" of a and b is:")
+print(sub)
+print("The \"product\" of a and b is:")
+print(product)
+print("The \"division\" of a and b is:")
+print(division)
+print("The \"modulus\" of and b is:")
+print(modulus)
+print("the \"exponential\" of and b is:")
+print(exponential)
+print("The \"floor division\" of and b is:")
+print(floor_division)
